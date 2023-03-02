f38e7a4178d060e14a46bdf808a38ac8d247727f
StebDev <steblyuk.kostya@gmail.com>
2023-03-02 14:11:17 +0200

    instal laravel + vue

---
+++ resources/js/Page/app.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>
+        Laravel vite with vue 3
+    </h1>
+</template>
---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue';
+
+import app from './Page/app.vue'
+
+createApp(app).mount("#app")
